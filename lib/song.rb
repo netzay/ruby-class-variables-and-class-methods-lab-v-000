15e0b302e261a14aa03101d8990be8521f18e730
Ally Netzel <netzelally@gmail.com>
2018-04-21 14:48:40 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,11 +1,17 @@
 require "pry"
 class Song 
+<<<<<<< HEAD
   attr_accessor :name, :artist, :genre 
+=======
+  #attr_accessor :name, :artist, :genre 
+  #binding.pry
+>>>>>>> d44c55f3835d81d46fe4d32cfdd7f82f058a7db4
   @@count = 0
   @@genres = []
   @@artists = []
   
   def initialize(name, artist, genre)
+<<<<<<< HEAD
     @@count += 1 
     @name = name
     @genre = genre
@@ -52,3 +58,37 @@ end
 end
       
       
+=======
+    @@name = :name
+    @@count += 1 
+    @@genres = :genre
+    #binding.pry
+    @@artists = :artist
+  end 
+  
+  def count
+    @@count.count
+  end 
+  
+  def genres 
+    if !@@genres.include?(genre)
+      @@genres << genre
+    end
+    @@genres
+  end 
+  
+  def artists
+    if !@@artists.include(artist)
+      @@artists << artist
+    end
+    @@artist
+  end
+
+  def genre_count
+    hash = Hash.new 
+    hash = @@genres.each {|v,k| puts v[k] }
+  hash
+  end
+  
+end 
+>>>>>>> d44c55f3835d81d46fe4d32cfdd7f82f058a7db4
